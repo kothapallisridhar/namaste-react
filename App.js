6bd63de1c77ee1d9d0674fafe8c5622120e94559
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-14 23:19:05 +0530

    text update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "child1" }, "Saibaba Sarath Babuji!!!"),
+  React.createElement("h1", { id: "child1" }, "Saibaba Sarath Babuji!"),
   React.createElement("h2", { id: "child2" }, "Shirdi"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
