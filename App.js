20188ac64bc9ddaec0931144267edebefa602f79
Sridhar Kothapalli <sridr8@gmail.com>
2023-10-14 16:34:02 +0530

    added App.js file and css

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Sri Sai Sarath!!!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
