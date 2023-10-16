ba6da897d5d1465f51a575f3a24a9c8f88e8fbde
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-16 12:58:36 +0530

    JSX

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "child1" }, "Saibaba Sarath Babuji!"),
-  React.createElement("h2", { id: "child2" }, "Shirdi"),
-]);
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement("h1", { id: "child1" }, "Saibaba Sarath Babuji!"),
+//   React.createElement("h2", { id: "child2" }, "Shirdi"),
+// ]);
+
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
