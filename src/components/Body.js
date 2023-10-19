85771e5164905ede4f3ca38927e7cbc5867a5003
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-19 21:52:22 +0530

    restaurant data added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 
-const resData = [
+const resList = [
   {
     info: {
       id: "375041",
@@ -1767,24 +1767,9 @@ const resData = [
 const Body = () => {
   return (
     <div className="body">
-      <RestaurantCard
-        image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x4uyxvihmg8qa3pddkgf"
-        resName="Meghana Foods"
-        cuisine="South Indian, Asian"
-        rating="4.4 stars"
-      />
-      <RestaurantCard
-        image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/emlehbuwgsmryxhwzhvq"
-        resName="Lavonne"
-        cuisine="Bakery, Desserts"
-        rating="4.5 stars"
-      />
-      <RestaurantCard
-        image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/v9fczzlua1wnrx63fh1g"
-        resName="Soul Rasa"
-        cuisine="Indian, Healthy Food"
-        rating="4.6 stars"
-      />
+      {resList.map((restaurant) => (
+        <RestaurantCard resData={restaurant} />
+      ))}
     </div>
   );
 };
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,10 +1,19 @@
-const RestaurantCard = ({ image, resName, cuisine }) => {
+const RestaurantCard = (props) => {
+  //console.log(resCard);
+  const { resData } = props;
+
+  console.log(resData);
   return (
     <div className="card">
-      <img src={image} />
-      <h4>{resName}</h4>
-      <h5>{cuisine}</h5>
-      <h5>4.4 stars</h5>
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData?.info.cloudinaryImageId
+        }
+      />
+      <h4>{resData?.info.name}</h4>
+      <h5>{resData?.info.cuisines.join(", ")}</h5>
+      <h5>{resData?.info.avgRating}</h5>
     </div>
   );
 };
