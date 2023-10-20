fe17c37bc7eceb0a1900971a7666d387b8af388b
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-20 14:12:20 +0530

    ustState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
-    <div className="body">
-      {resList.map((restaurant) => (
-        <RestaurantCard resData={restaurant} />
-      ))}
+    <div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4.3
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
+      <div className="body">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard resData={restaurant} />
+        ))}
+      </div>
     </div>
   );
 };
