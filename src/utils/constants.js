1eb6704c7dd64218dbc6bab933bee5362e8a68ed
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-20 08:57:46 +0530

    mixing named and default export import

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy74aAasUvsShzcGg5nTMO9Nd34w09s9wmQg&usqp=CAU";
 
-export const CDN_URL =
+const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export default CDN_URL;
