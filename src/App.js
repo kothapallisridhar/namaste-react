27a69b9e43e016c3ac63bffedea42b5566b403aa
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-24 18:05:37 +0530

    Error component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // const heading = React.createElement("div", { id: "parent" }, [
