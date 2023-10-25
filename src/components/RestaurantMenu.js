0ce2bc1bbefe327ae7c0cd3c276783197b11cc18
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-25 12:28:11 +0530

    RestaurantMenu data added

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,30 +1,35 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-import { MENU_URL } from "../utils/constants";
 const RestaurantMenu = () => {
-  //const [resInfo, setResInfo] = useState(null);
+  const [resInfo, setResInfo] = useState(null);
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(MENU_URL + 280216);
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=280216"
+    );
     const json = await data.json();
-    console.log(json);
+    //console.log(json.data.cards[0].card.card.info);
+    setResInfo(json.data);
   };
 
-  //if (resInfo === null) return <Shimmer />;
+  //const { cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
+  const name = resInfo?.cards[0]?.card?.card?.info?.name;
+  const cuisines = resInfo?.cards[0]?.card?.card?.info?.cuisines;
+  const costForTwoMessage =
+    resInfo?.cards[0]?.card?.card?.info?.costForTwoMessage;
+
+  if (resInfo === null) return <Shimmer />;
 
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
-      <ul>
-        <li>Biryani</li>
-        <li>Kichidi</li>
-        <li>Aloo 65</li>
-      </ul>
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
     </div>
   );
 };
