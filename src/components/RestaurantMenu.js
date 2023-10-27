958d79998bac4b602536dca6c05d032e882535f5
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-27 13:04:06 +0530

    logging data

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -12,7 +12,7 @@ const RestaurantMenu = () => {
       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=280216"
     );
     const json = await data.json();
-    //console.log(json.data);
+    console.log(json.data);
     setResInfo(json.data);
   };
 
