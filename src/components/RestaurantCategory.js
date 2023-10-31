a6e925c94d68c8cd8d43a26c5b1423ac9b92d62d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-10-31 18:38:06 +0530

    RestaurantCategory

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,9 +1,11 @@
 const RestaurantCategory = ({ data }) => {
   console.log("data in category", data);
   return (
-    <div>
-      <span>{data?.title}</span>
-      <span>{"⬇️"}</span>
+    <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between">
+      <span className="font-bold">
+        {data?.title} ({data?.itemCards?.length})
+      </span>
+      <span>⬇️</span>
     </div>
   );
 };
