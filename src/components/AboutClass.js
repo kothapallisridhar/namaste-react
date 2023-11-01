ccb22fb55d5450e3f54c8f3fb0548962918d9296
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-01 09:38:39 +0530

    using Context in a Class Component

--- src/components/AboutClass.js
+++ src/components/AboutClass.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 class AboutClass extends React.Component {
   constructor(props) {
@@ -23,7 +24,15 @@ class AboutClass extends React.Component {
     console.log("render");
     return (
       <div>
-        <h1>About us</h1>
+        <h1>About Class Component</h1>
+        <div>
+          LoggedIn User
+          <UserContext.Consumer>
+            {({ loggedInUser }) => (
+              <h1 className="font-bold text-xl">{loggedInUser}</h1>
+            )}
+          </UserContext.Consumer>
+        </div>
         <h2>About us page</h2>
         <UserClass name={"Sridhar"} location={"Munich"} component={"Class"} />
       </div>
