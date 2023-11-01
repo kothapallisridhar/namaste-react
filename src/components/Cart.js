2c0f6fcbc7f3eb9db0ab095a9906f5b7bd280a5b
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-01 18:33:14 +0530

    Cart items added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,16 @@
+import { useSelector } from "react-redux";
+import ItemList from "./ItemList";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartItems = useSelector((store) => store.cart.items);
+  return (
+    <div className="m-4 p-4 text-center">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        <ItemList items={cartItems} />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
