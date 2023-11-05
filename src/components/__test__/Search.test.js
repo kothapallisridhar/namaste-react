911197795f2a4e8f1a52c5ece3c0bad7f3b4ff49
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-05 23:23:04 +0530

    test: render Body Component with Search

--- src/components/__test__/Search.test.js
+++ src/components/__test__/Search.test.js
@@ -1,11 +1,12 @@
 import { render } from "@testing-library/react";
 import Body from "../Body";
+import MOCK_DATA from "../mocks/mockResListData";
 
 it("Should render the Body Component with Search", () => {
   global.fetch = jest.fn(() => {
     return Promise.resolve({
       json: () => {
-        return Promise.resolve(data);
+        return Promise.resolve(MOCK_DATA);
       },
     });
   });
