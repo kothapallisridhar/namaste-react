c532539ebe86b338a3d17551d5335a73c9d182fc
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-05 20:31:58 +0530

    RestaurantCard.test.js

---
+++ src/components/__test__/RestaurantCard.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import RestaurantCard from "../RestaurantCard";
+
+it("Should render RestaurantCard Component with props data", () => {
+  render(<RestaurantCard />);
+});
