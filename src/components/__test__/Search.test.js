55bb26fab358c655f24c5e283bb4c85061871fd4
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-05 23:34:15 +0530

    watch-test script

--- src/components/__test__/Search.test.js
+++ src/components/__test__/Search.test.js
@@ -1,8 +1,10 @@
 import { render } from "@testing-library/react";
 import Body from "../Body";
 import MOCK_DATA from "../mocks/mockResListData";
+import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
 
-it("Should render the Body Component with Search", () => {
+it("Should render the Body Component with Search", async () => {
   global.fetch = jest.fn(() => {
     return Promise.resolve({
       json: () => {
@@ -10,6 +12,11 @@ it("Should render the Body Component with Search", () => {
       },
     });
   });
-
-  render(<Body />);
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
 });
