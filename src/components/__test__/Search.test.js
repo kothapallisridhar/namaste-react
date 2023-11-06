fd46c476f9d1f9bb6317a7c23f27b68b6d5a57b4
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-06 12:34:32 +0530

    test case: Search button

--- src/components/__test__/Search.test.js
+++ src/components/__test__/Search.test.js
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Body from "../Body";
 import MOCK_DATA from "../mocks/mockResListData";
 import { act } from "react-dom/test-utils";
 import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 it("Should render the Body Component with Search", async () => {
   global.fetch = jest.fn(() => {
@@ -19,4 +20,6 @@ it("Should render the Body Component with Search", async () => {
       </BrowserRouter>
     )
   );
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  expect(searchBtn).toBeInTheDocument();
 });
