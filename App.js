22e764d54e401cfb5acc4116bc381140c2aaaed5
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-15 23:35:43 +0530

    add css

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const heading = React.createElement("h1", {}, "Hello React World");
+const heading = React.createElement("h1", {id: "heading"}, "Hello React World");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
