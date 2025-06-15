6fb7e45f9913fb87f810e97df4a11a4549f504ab
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-15 23:47:56 +0530

    nest elements

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
+
+const parent = React.createElement("div", 
+    {id: "parent"}, 
+    React.createElement("div", {id:"child"},
+        React.createElement("h1", {}, "I am a H1 tag")
+    )
+)
+
+
 const heading = React.createElement("h1", {id: "heading"}, "Hello React World");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
