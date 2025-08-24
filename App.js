5578cc28fdd45764e921b8d3e54e04d38f042247
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-24 22:06:13 +0530

    refactor

--- App.js
+++ App.js
@@ -2,8 +2,9 @@
 const parent = React.createElement("div", 
     {id: "parent"}, 
     React.createElement("div", {id:"child"},
-        [React.createElement("h1", {id: "heading"}, "I am a H1 tag"),
-            React.createElement("h2", {}, "I am a H2 tag")
+        [
+            React.createElement("h1", {id: "heading"}, "I am a H1 tag"),
+            React.createElement("h2", {id: "heading2"}, "I am a H2 tag")
         ]
     )
 )
