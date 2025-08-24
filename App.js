11ce6b04a2134434e7f5b9af1edd06779f7c2c1c
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-24 21:27:59 +0530

    update css

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 const parent = React.createElement("div", 
     {id: "parent"}, 
     React.createElement("div", {id:"child"},
-        [React.createElement("h1", {}, "I am a H1 tag"),
+        [React.createElement("h1", {id: "heading"}, "I am a H1 tag"),
             React.createElement("h2", {}, "I am a H2 tag")
         ]
     )
