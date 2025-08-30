7b7369f9d45bad880022e57c821908bc0eb39fa0
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-30 13:12:43 +0530

    refactor

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { jsx } from 'react/jsx-runtime';
 
 // React Element
-// React.createElement => Object => HTMLElement(render)
+// React.createElement => ReactElement-JS Object => HTMLElement(render)
 
 const heading = React.createElement("h1", {id: "heading"}, "This is a React Element 🚀");
 console.log("heading:", heading);
-// JSX 
-const jsxHeading = <h1 id='heading'>Namaste React using JSX ✈️ 🏎️ 🏡 💰 💶 💵 💷 💴 🧧 💸 🪐</h1>
+
+// JSX - HTML-like or XML-like syntax
+// JSX (transpiled before is reaches JS) - PARCEL - Babel
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+const jsxHeading = <h1 id='heading'>🪐 Namaste React using JSX ✈️ 🏎️ 🏡</h1>
 console.log("jsxHeading:", jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
